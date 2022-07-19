ab13f3d9e6d3015b3b2e7163813c9e28ce5f33e9
rohith57 <89677980+rohith57@users.noreply.github.com>
2022-07-19 10:25:06 +0530

    Add files via upload

--- pysheets.py
+++ pysheets.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 
 import os.path
+import google.auth.transport.requests
+import requests
 
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -22,7 +23,7 @@ creds = service_account.Credentials.from_service_account_file(
 
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '122JpWqlqbvlni5EodBuNwiXqjzBfAx8aNzBSX35o3h0'
+SAMPLE_SPREADSHEET_ID = '1RHLjYx5pj64ikRjL5tLguq8fplG54Wrulz6e_tvhcaw'
 
 
 
@@ -37,14 +38,46 @@ service = build('sheets', 'v4', credentials=creds)
 
         # Call the Sheets API
 sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range="Sheet1!A1:G6").execute()
-values = result.get('values', [])
-lst = [element for innerList in values for element in innerList]
+#result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                   # range="Sheet1!A1:C6").execute()
+#values = result.get('values', [])
+#lst = [element for innerList in values for element in innerList]
 
-def Convert(lst):
+#def Convert(lst):
+    #res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
+    #return res_dct
+
+#print(Convert(lst))
+
+result =  sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range="Sheet1!A1:B6").execute()
+values = result.get('values',[])
+lst = [element for innerlist in values for element in innerlist]
+def rackA(lst):
     res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
-    return res_dct
+    print(res_dct) 
+result1 = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range="Sheet1!C1:D6").execute()
 
-print(Convert(lst))
+values1 = result1.get('values',[])
+lst1 = [element for innerlist in values1 for element in innerlist]
+def rackB(lst1):
+    res_dect1 = {lst1[i]: lst1[i+1] for i in range(0, len(lst1),2)}
+    print(res_dect1)
+
+
+result2 = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range="Sheet1!E1:F6").execute()
+
+values2 = result2.get('values',[])
+lst2 = [element for innerlist in values2 for element in innerlist]
+def rackC(lst2):
+    res_dect2 = {lst2[i]: lst2[i+1] for i in range(0, len(lst2),2)}
+    print(res_dect2)
+
+rackA(lst)
+
+
+
+
 
